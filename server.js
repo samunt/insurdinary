43b1faee6d006924cd65d65fb20992e051e4af42
samunt <sammy.unterman@gmail.com>
2020-08-16 17:30:47 -0400

    firebase service

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const admin = require('firebase-admin');
-const serviceAccount = require("./firebaseServiceAccount.service.json");
+const serviceAccount = require("./firebaseService/firebaseService/firebaseServiceAccount.service.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
