c7ea277054255d3bb83bc2136aaa4d4d5266cba9
samunt <sammy.unterman@gmail.com>
2020-08-19 11:54:57 -0400

    family income modals

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "51210819adcf86ed9b75";
+/******/ 	var hotCurrentHash = "df7f34029763787aea53";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/FamilyIncomeForm.js
+++ components/FamilyIncomeForm.js
@@ -22,8 +22,12 @@ export default function FamilyIncomeForm() {
     const [spouseIncome, setSpouseIncome] = useState('');
     const [showFamilyIncome, setShowFamilyIncome] = useState(false);
     const handleCloseFamilyIncome = () => setShowFamilyIncome(false);
+    const [showWhatAboutYourOtherIncome, setShowWhatAboutYourOtherIncome] = useState(false);
+    const handleCloseWhatAboutYourOtherIncome = () => setShowWhatAboutYourOtherIncome(false);
+    const [showWhyDoWeAsk, setShowWhyDoWeAsk] = useState(false);
+    const handleCloseWhyDoWeAsk = () => setShowWhyDoWeAsk(false);
 
-
+    // setShowWhatAboutYourOtherIncome
     useEffect(() => {
         dispatch({ type: "YOUR_INCOME", yourIncome: yourIncome });
     }, [yourIncome]);
@@ -133,6 +137,40 @@ export default function FamilyIncomeForm() {
                         </Button>{" "}
                     </Col>
                 </Row>
+                <Row>
+                    <Col
+                        xs={{ span: 11 }}
+                        md={{ span: 6, offset: 3 }}
+                        lg={{ span: 6, offset: 3 }}
+                        onClick={() => {setShowWhatAboutYourOtherIncome(true)}}
+                    >
+                        <h5 className={styles.header}><u>What about investments and other income?</u></h5>
+                    </Col>
+                    <Col xs={1} md={1} lg={1}>
+                        <QuestionCircle
+                            size={25}
+                            onClick={() => {setShowWhatAboutYourOtherIncome(true)}}
+                            style={{position: 'relative', top: '12px', right: '35px'}}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col
+                        xs={{ span: 11 }}
+                        md={{ span: 6, offset: 3 }}
+                        lg={{ span: 6, offset: 3 }}
+                        onClick={() => {setShowWhyDoWeAsk(true)}}
+                    >
+                        <h5 className={styles.header}><u>Why do we ask about your income?</u></h5>
+                    </Col>
+                    <Col xs={1} md={1} lg={1}>
+                        <QuestionCircle
+                            size={25}
+                            onClick={() => {setShowWhyDoWeAsk(true)}}
+                            style={{position: 'relative', top: '12px', right: '35px'}}
+                        />
+                    </Col>
+                </Row>
             </Form>
             {/*ask about family income*/}
             <Modal show={showFamilyIncome} onHide={handleCloseFamilyIncome}>
@@ -143,6 +181,29 @@ export default function FamilyIncomeForm() {
                     </Button>
                 </Modal.Footer>
             </Modal>
+            {/*ask about other income*/}
+            <Modal show={showWhatAboutYourOtherIncome} onHide={handleCloseWhatAboutYourOtherIncome}>
+                <Modal.Body>Please don’t include income that you generate from investments. We will cover your investments in the “savings” section. Some examples of income generated from investments are dividends, interest payments and capital gains collected from the sale of an asset.
+
+                    Similarly, don’t include any income that isn’t tied to your work and would still be generated if you passed away. Some examples of non-employment income are rental income on a property you own or even income you make off a business that isn’t tied to your time.</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="outline-primary" onClick={handleCloseWhatAboutYourOtherIncome}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            {/*ask about other income*/}
+            <Modal show={showWhyDoWeAsk} onHide={handleCloseWhyDoWeAsk}>
+                <Modal.Body>It’s important for us to have an accurate understanding of your finances, and your income is a key aspect of your financial situation. Our calculations will use your income (along with many other factors) to evaluate how much life insurance protection your family needs. The same goes for your partner’s income.
+
+                    We never recommend more coverage than you need. This prevents you from becoming “overprotected,” keeping your costs low and freeing up money for you and your family. There are many features in our calculation that help us do this. We include your partner’s future income as money that will be available to pay for some of your family’s future expenses.</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="outline-primary" onClick={handleCloseWhyDoWeAsk}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     )
 }
