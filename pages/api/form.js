718ece369a484fa22ccd6df99612c84edc62a6ba
samunt <sammy.unterman@gmail.com>
2020-08-24 09:09:43 -0400

    trying to make the database work on the post call

--- pages/api/form.js
+++ pages/api/form.js
@@ -1,37 +1,33 @@
-// import bodyParser from "body-parser";
-//
-// export const config = {
-//     api: {
-//         bodyParser: false,
-//     },
-// };
+
 export default (req, res) => {
+    // get database info for firebase
     const serviceAccount = require("../../firebaseService/firebaseServiceAccount.service.json");
     const admin = require('firebase-admin');
-
+    // initialize firebase
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
         databaseURL: "https://insurdinary-a02d7.firebaseio.com"
     });
-
+    // set up database references
     let db = admin.database();
     let date = new Date();
     let dateToString = date.toString();
     let formStore = db.ref("formStore");
-
+    // build the path for the object to be stored
     let pageRefFormStore = formStore.child('form/' + req.body.form.id + '__' + dateToString);
 
     if (req.method === 'POST') {
-        res.json(req.body);
         try {
+            // push the data to the database
             pageRefFormStore.set(req.body.form);
+            res.setHeader('Content-Type', 'application/json');
+            res.json(req.body);
+
         } catch(error) {
             console.log('ERROR===>', error)
         }
-
     } else {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify({ response: 'Invalid Request' }))
-        console.log('not a post')
     }
 }
